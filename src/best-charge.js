3710ff72d47da21548950ed1261d12857d6d62b9
GUOED3 <EDDY.GUO@OOCL.COM>
2019-07-11 13:04:32 +0800

    rebuild code (I)

--- src/best-charge.js
+++ src/best-charge.js
@@ -1,23 +1,40 @@
 const loadAllItems = require('../src/items');
 const loadPromotions = require('../src/promotions');
 
+const TITLE = '============= 订餐明细 =============\n';
+const LINE = '-----------------------------------\n'
+const EDN_LINE = '==================================='
+
 function bestCharge(selectedItems) {
   const orderList = createOrder(selectedItems);
-  const discount = getDiscount(orderList);
-  let result = '============= 订餐明细 =============\n';
-  orderList.forEach(order => {
-    result += order.name + ' x ' + order.num +' = ' + order.num * order.price + '元\n';
-  })
-  result += '-----------------------------------\n';
+  const discount = chooseDiscount(orderList);
+  let result = TITLE;
+  result += printItemString(orderList);
+  result += LINE;
   if(discount.mode !== ''){
-    result += '使用优惠:\n' + discount.mode;
-    if(discount.mode === '指定菜品半价'){
-      let itemString = discount.items.join('，');
-      result += '(' + itemString +')';
-    }
-    result += '，省' + discount.offerPrice + '元\n-----------------------------------\n';
+    result = printPromotionString(discount,result);
   }
-  result += '总计：' + (discount.total - discount.offerPrice) + '元\n===================================';
+  result += `总计：${discount.total - discount.offerPrice}元\n`;
+  result += EDN_LINE;
+  return result;
+}
+
+const printItemString = orderList => {
+  let result = '';
+  orderList.forEach(order => {
+    result += `${order.name} x ${order.num} = ${order.num * order.price}元\n`;
+  })
+  return result;
+}
+
+const printPromotionString = (discount,result) => {
+  result += `使用优惠:\n${discount.mode}`;
+  if(discount.mode === '指定菜品半价'){
+    let itemString = discount.items.join('，');
+    result += `(${itemString})`;
+  }
+  result += `，省${discount.offerPrice}元\n`;
+  result += LINE;
   return result;
 }
 
@@ -25,7 +42,6 @@ const isItemExist = selectedItems => {
   let flag = true;
   const items = loadAllItems();
   const idList = items.map(item => item['id']);
-
   selectedItems.forEach(item => {
     if(idList.indexOf(item) === -1){
       flag = false;
@@ -42,12 +58,15 @@ const createOrder = selectedItems => {
     let index = item.indexOf('x');
     let id = item.substring(0,index-1);
     let pos = idList.indexOf(id);
-    order.push({id:items[pos].id,name:items[pos].name,num:Number(item.substring(index+1,item.length)),price:items[pos].price});
+    order.push({id:items[pos].id,
+      name:items[pos].name,
+      num:Number(item.substring(index+1,item.length)),
+      price:items[pos].price});
   })
   return order;
 }
 
-const getDiscount = orderList => {
+const chooseDiscount = orderList => {
   const orderIdList = orderList.map(order => order['id']);
   let totalFirst = 0;
   let total = 0;
@@ -86,6 +105,4 @@ const getDiscount = orderList => {
   return discount;
 }
 
-module.exports = {isItemExist,createOrder,getDiscount,bestCharge};
-
-
+module.exports = {isItemExist,createOrder,chooseDiscount,bestCharge};
